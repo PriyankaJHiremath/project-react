0229717b29625530926abdc6fdee4af66259dcf6
PriyankaJHiremath <priyanka.hiremath@byjus.com>
2021-02-28 15:41:00 +0530

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './component/Greet';
+import Welcome from './component/Welcome';
 
 function App() {
   return (
     <div className="App">
      <Greet/>
+     <Welcome/>
     </div>
   );
 }
